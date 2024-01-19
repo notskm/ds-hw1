ca0008aa6383a7b8eceaae151e2e94ed953ef5b7
notskm <41784014+notskm@users.noreply.github.com>
2024-01-18 20:12:35 -0500

    Establish basic connection with sockets

--- src/csx55/overlay/node/MessagingNode.java
+++ src/csx55/overlay/node/MessagingNode.java
@@ -1,5 +1,25 @@
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.io.IOException;
+
 public class MessagingNode {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            Socket registrySocket = new Socket(registryHost, registryPort);
+            registrySocket.close();
+        }
+        catch(UnknownHostException e) {
+            System.err.println("Unknown host: " + registryHost);
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        catch(IOException e) {
+            System.err.println("Unable to reach registry at " + registryHost + ":" + registryPort);
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
     }
+    
+    static private String registryHost = "127.0.0.1";
+    static private int registryPort = 5000;
 }
--- src/csx55/overlay/node/Registry.java
+++ src/csx55/overlay/node/Registry.java
@@ -1,7 +1,39 @@
 package csx55.overlay.node;
 
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+
 public class Registry {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ServerSocket serverSocket = openServerSocket();
+        Socket nodeSocket = listenForConnections(serverSocket);
     }
+    
+    public static ServerSocket openServerSocket() {
+        try {
+            return new ServerSocket(serverPort);
+        }
+        catch(IOException e) {
+            System.err.println("Unable to start registry on port " + serverPort);
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+        return null;
+    }
+    
+    public static Socket listenForConnections(ServerSocket socket) {
+        while(true) {
+            try {
+                Socket connection = socket.accept();
+                return connection;
+            }
+            catch(IOException e) {
+                System.err.println("Unable to accept connection");
+                System.err.println(e.getMessage());
+            }
+        }
+    }
+    
+    static private int serverPort = 5000;
 }
