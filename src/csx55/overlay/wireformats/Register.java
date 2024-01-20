7800fb430a0d9bea8149b37483a4087f5752a395
notskm <41784014+notskm@users.noreply.github.com>
2024-01-20 17:52:32 -0500

    Make Register serializable

--- src/csx55/overlay/wireformats/Register.java
+++ src/csx55/overlay/wireformats/Register.java
@@ -1,7 +1,31 @@
 package csx55.overlay.wireformats;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
 public class Register {
     private String ipAddress;
     private int portNumber;
+    
+    public Register(byte[] marshalledBytes) throws IOException {
+        ByteArrayInputStream baInputStream = new ByteArrayInputStream(marshalledBytes);
+        DataInputStream din = new DataInputStream(new BufferedInputStream(baInputStream));
+        
+        int ipAddressLength = din.readInt();
+        byte[] ipAddressBytes = new byte[ipAddressLength];
+        din.readFully(ipAddressBytes);
+        
+        ipAddress = new String(ipAddressBytes);
+        portNumber = din.readInt();
+        
+        baInputStream.close();
+        din.close();
+    }
     
     public Register(String ipAddress, int portNumber) {
         this.ipAddress = ipAddress;
@@ -14,5 +38,22 @@ public class Register {
     
     public int getPortNumber() {
         return portNumber;
+    }
+    
+    public byte[] getBytes() throws IOException {
+        ByteArrayOutputStream baOutputStream = new ByteArrayOutputStream();
+        DataOutputStream dout = new DataOutputStream(new BufferedOutputStream(baOutputStream));
+        
+        dout.writeInt(ipAddress.length());
+        dout.writeBytes(ipAddress);
+        dout.writeInt(portNumber);
+        
+        dout.flush();
+        byte[] marshalledBytes = baOutputStream.toByteArray();
+        
+        baOutputStream.close();
+        dout.close();
+        
+        return marshalledBytes;
     }
 }
