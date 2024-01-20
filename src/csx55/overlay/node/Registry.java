a3d405bd843ad713573b56a8e60f1ae387d15af3
notskm <41784014+notskm@users.noreply.github.com>
2024-01-20 17:54:01 -0500

    Send a Register object from MessagingNode to Registry

--- src/csx55/overlay/node/Registry.java
+++ src/csx55/overlay/node/Registry.java
@@ -3,11 +3,29 @@ package csx55.overlay.node;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.io.IOException;
+import java.io.DataInputStream;
+import csx55.overlay.wireformats.Register;
 
 public class Registry {
     public static void main(String[] args) {
         ServerSocket serverSocket = openServerSocket();
         Socket nodeSocket = listenForConnections(serverSocket);
+
+        try {
+            DataInputStream in = new DataInputStream(nodeSocket.getInputStream());
+
+            byte[] data = new byte[in.readInt()];
+            in.readFully(data);
+            Register register = new Register(data);
+
+            System.out.println(register.getIpAddress());
+            System.out.println(register.getPortNumber());
+            in.close();
+        }
+        catch(IOException e) {
+            System.err.println("Unable to read from socket");
+            System.err.println(e.getMessage());
+        }
     }
     
     public static ServerSocket openServerSocket() {
