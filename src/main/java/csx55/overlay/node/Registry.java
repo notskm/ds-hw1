6f92143d0394380f66c547079d809d9bc5cc71ef
notskm <41784014+notskm@users.noreply.github.com>
2024-01-23 23:20:52 -0500

    Format Registry

--- src/main/java/csx55/overlay/node/Registry.java
+++ src/main/java/csx55/overlay/node/Registry.java
@@ -23,50 +23,46 @@ public class Registry {
             System.out.println(register.getIpAddress());
             System.out.println(register.getPortNumber());
             in.close();
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             System.err.println("Unable to read from socket");
             System.err.println(e.getMessage());
         }
     }
-    
+
     public static ServerSocket openServerSocket() {
         try {
             return new ServerSocket(serverPort);
-        }
-        catch(IOException e) {
+        } catch (IOException e) {
             System.err.println("Unable to start registry on port " + serverPort);
             System.err.println(e.getMessage());
             System.exit(1);
         }
         return null;
     }
-    
+
     public static Socket listenForConnections(ServerSocket socket) {
-        while(true) {
+        while (true) {
             try {
                 Socket connection = socket.accept();
                 return connection;
-            }
-            catch(IOException e) {
+            } catch (IOException e) {
                 System.err.println("Unable to accept connection");
                 System.err.println(e.getMessage());
             }
         }
     }
-    
+
     private static void parseArgs(String[] args) {
-        if(args.length >= 1) {
+        if (args.length >= 1) {
             try {
                 serverPort = Integer.parseInt(args[0]);
-            }
-            catch(NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 System.err.println("Invalid port number: " + args[0]);
                 System.err.println(e.getMessage());
                 System.exit(1);
             }
         }
     }
-    
+
     static private int serverPort = 5000;
 }
