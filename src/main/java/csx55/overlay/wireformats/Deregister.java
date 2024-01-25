54a24a61de67f97161a8c0b15a0187ee7b0c1663
notskm <41784014+notskm@users.noreply.github.com>
2024-01-25 01:05:41 -0500

    Make Deregister an Event type

--- src/main/java/csx55/overlay/wireformats/Deregister.java
+++ src/main/java/csx55/overlay/wireformats/Deregister.java
@@ -1,5 +1,22 @@
 package csx55.overlay.wireformats;
 
-public class Deregister {
+import java.io.IOException;
+
+public class Deregister implements Event {
+    public Deregister() {
+        
+    }
+
+    @Override
+    public byte[] getBytes() throws IOException {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBytes'");
+    }
+
+    @Override
+    public int getType() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getType'");
+    }
     
 }
--- src/test/java/csx55/overlay/wireformats/TestDeregister.java
+++ src/test/java/csx55/overlay/wireformats/TestDeregister.java
@@ -1,4 +1,11 @@
 package csx55.overlay.wireformats;
 
+import org.junit.jupiter.api.Test;
+
 public class TestDeregister {
+    @Test
+    void testDeregisterIsAnEvent() {
+        Deregister deregister = new Deregister();
+        assert(deregister instanceof Event);
+    }
 }
