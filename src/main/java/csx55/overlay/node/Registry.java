1e19799eb16ff34c65b3d82fe99586e0b8e62466
notskm <41784014+notskm@users.noreply.github.com>
2024-01-28 21:47:15 -0500

    Use TCPReceiverThread in Registry

--- src/main/java/csx55/overlay/node/Registry.java
+++ src/main/java/csx55/overlay/node/Registry.java
@@ -1,19 +1,23 @@
 package csx55.overlay.node;
 
 import java.net.Socket;
+import java.util.ArrayList;
 import java.net.ServerSocket;
 import java.io.IOException;
-import java.io.DataInputStream;
 
+import csx55.overlay.transport.TCPReceiverThread;
 import csx55.overlay.transport.TCPServerThread;
 import csx55.overlay.wireformats.Event;
-import csx55.overlay.wireformats.EventFactory;
 import csx55.overlay.wireformats.Protocol;
 import csx55.overlay.wireformats.Register;
 
 public class Registry {
+    private static ArrayList<TCPReceiverThread> receiverThreads;
+
     public static void main(String[] args) {
         parseArgs(args);
+        
+        receiverThreads = new ArrayList<>();
 
         try {
             TCPServerThread serverThread = createServerThread(serverPort);
@@ -23,7 +27,16 @@ public class Registry {
                 Socket nodeSocket = serverThread.poll();
 
                 if (nodeSocket != null) {
-                    readAndTriggerEvent(nodeSocket);
+                    TCPReceiverThread thread = new TCPReceiverThread(nodeSocket);
+                    receiverThreads.add(thread);
+                    thread.start();
+                }
+                
+                for(TCPReceiverThread thread : receiverThreads) {
+                    Event event = thread.poll();
+                    if(event != null) {
+                        onEvent(event);
+                    }
                 }
             }
         } catch (IOException e) {
@@ -35,24 +48,6 @@ public class Registry {
         ServerSocket socket = new ServerSocket(port);
         TCPServerThread thread = new TCPServerThread(socket);
         return thread;
-    }
-
-    private static void readAndTriggerEvent(Socket nodeSocket) {
-        try {
-            Event event = readData(nodeSocket);
-            onEvent(event);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-    
-    private static Event readData(Socket nodeSocket) throws IOException {
-        DataInputStream in = new DataInputStream(nodeSocket.getInputStream());
-        byte[] data = new byte[in.readInt()];
-        in.readFully(data);
-        in.close();
-
-        return EventFactory.getInstance().getEvent(data);
     }
 
     private static void onEvent(Event event) {
@@ -68,18 +63,6 @@ public class Registry {
     private static void onRegisterRequest(Register register) {
         System.out.println(register.getIpAddress());
         System.out.println(register.getPortNumber());
-    }
-
-    public static Socket listenForConnections(ServerSocket socket) {
-        while (true) {
-            try {
-                Socket connection = socket.accept();
-                return connection;
-            } catch (IOException e) {
-                System.err.println("Unable to accept connection");
-                System.err.println(e.getMessage());
-            }
-        }
     }
 
     private static void parseArgs(String[] args) {
