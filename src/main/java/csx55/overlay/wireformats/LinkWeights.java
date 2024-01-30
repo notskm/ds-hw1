b52a848cd7c4c8dbb3494348c77832a5ced26eea
notskm <41784014+notskm@users.noreply.github.com>
2024-01-30 15:44:55 -0500

    Implement getters

--- src/main/java/csx55/overlay/wireformats/LinkWeights.java
+++ src/main/java/csx55/overlay/wireformats/LinkWeights.java
@@ -3,6 +3,39 @@ package csx55.overlay.wireformats;
 import java.io.IOException;
 
 public class LinkWeights implements Event {
+    String hostnameA;
+    String hostnameB;
+    int portNumberA;
+    int portNumberB;
+    int linkWeight;
+
+    public LinkWeights(String ipA, int portA, String ipB, int portB, int weight) {
+        hostnameA = ipA;
+        hostnameB = ipB;
+        portNumberA = portA;
+        portNumberB = portB;
+        linkWeight = weight;
+    }
+
+    public String getHostnameA() {
+        return hostnameA;
+    }
+
+    public String getHostnameB() {
+        return hostnameB;
+    }
+
+    public int getPortA() {
+        return portNumberA;
+    }
+
+    public int getPortB() {
+        return portNumberB;
+    }
+
+    public int getWeight() {
+        return linkWeight;
+    }
 
     @Override
     public byte[] getBytes() throws IOException {
@@ -14,5 +47,4 @@ public class LinkWeights implements Event {
     public int getType() {
         return Protocol.LINK_WEIGHTS.ordinal();
     }
-    
 }
--- src/test/java/csx55/overlay/wireformats/LinkWeightsTest.java
+++ src/test/java/csx55/overlay/wireformats/LinkWeightsTest.java
@@ -3,18 +3,54 @@ package csx55.overlay.wireformats;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LinkWeightsTest {
     @Test
     void testLinkWeightsIsAnEvent() {
-        LinkWeights linkWeights = new LinkWeights();
+        LinkWeights linkWeights = new LinkWeights("", 1, "", 1, 1);
         assert (linkWeights instanceof Event);
     }
-    
+
     @Test
     void testGetType() {
-        LinkWeights linkWeights = new LinkWeights();
+        LinkWeights linkWeights = new LinkWeights("", 1, "", 1, 1);
         assertEquals(Protocol.LINK_WEIGHTS.ordinal(), linkWeights.getType());
     }
-    
+
+    @ParameterizedTest
+    @ValueSource(strings = { "localhost", "127.0.0.1", "123.234.345.456" })
+    void testGetHostnameA(String hostname) {
+        LinkWeights linkWeights = new LinkWeights(hostname, 1, "", 1, 1);
+        assertEquals(hostname, linkWeights.getHostnameA());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "localhost", "127.0.0.1", "123.234.345.456" })
+    void testGetHostnameB(String hostname) {
+        LinkWeights linkWeights = new LinkWeights("", 1, hostname, 1, 1);
+        assertEquals(hostname, linkWeights.getHostnameB());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3, 4, 5 })
+    void testGetPortA(int port) {
+        LinkWeights linkWeights = new LinkWeights("", port, "", 1, 1);
+        assertEquals(port, linkWeights.getPortA());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3, 4, 5 })
+    void testGetPortB(int port) {
+        LinkWeights linkWeights = new LinkWeights("", 1, "", port, 1);
+        assertEquals(port, linkWeights.getPortB());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })
+    void testGetWeight(int weight) {
+        LinkWeights linkWeights = new LinkWeights("", 0, "", 0, weight);
+        assertEquals(weight, linkWeights.getWeight());
+    }
 }
