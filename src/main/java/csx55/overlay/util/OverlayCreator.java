78b05fb3a25b922e007ce65d824e015d1afe5c71
notskm <41784014+notskm@users.noreply.github.com>
2024-02-02 01:48:20 -0500

    Fix OverlayCreator

--- src/main/java/csx55/overlay/util/OverlayCreator.java
+++ src/main/java/csx55/overlay/util/OverlayCreator.java
@@ -10,16 +10,20 @@ import csx55.overlay.wireformats.MessagingNodeInfo;
 import csx55.overlay.wireformats.MessagingNodesList;
 
 public class OverlayCreator {
-    MessagingNodeInfo[] messagingNodes;
+    final ArrayList<MessagingNodeInfo> messagingNodes;
     ArrayList<LinkInfo> links;
     int[] connectionCounts;
     ArrayList<ArrayList<Integer>> connections;
-    int perNodeConnectionLimit;
+    final int perNodeConnectionLimit;
 
-    OverlayCreator(MessagingNodeInfo[] nodes, int connectionLimit) {
+    public OverlayCreator(ArrayList<MessagingNodeInfo> nodes, int connectionLimit) {
         messagingNodes = nodes;
-        connectionCounts = new int[nodes.length];
-        connections = new ArrayList<>(nodes.length);
+        connectionCounts = new int[nodes.size()];
+        connections = new ArrayList<>(nodes.size());
+        for (int i = 0; i < nodes.size(); i++) {
+            connections.add(new ArrayList<>());
+        }
+        links = new ArrayList<>();
         perNodeConnectionLimit = connectionLimit;
     }
 
@@ -31,8 +35,8 @@ public class OverlayCreator {
     private void connectNodes() {
         int jump = 1;
 
-        while (jump < messagingNodes.length) {
-            for (int node = 0; node < messagingNodes.length; node++) {
+        while (jump < messagingNodes.size()) {
+            for (int node = 0; node < messagingNodes.size(); node++) {
                 int nextNode = node + jump;
                 if (isAvailableForConnection(node) && isAvailableForConnection(nextNode)) {
                     connect(node, nextNode);
@@ -45,7 +49,7 @@ public class OverlayCreator {
     }
 
     private boolean isAvailableForConnection(int socketIndex) {
-        if (socketIndex > connectionCounts.length) {
+        if (socketIndex >= connectionCounts.length) {
             return false;
         }
         if (connectionCounts[socketIndex] >= perNodeConnectionLimit) {
@@ -62,8 +66,8 @@ public class OverlayCreator {
     }
 
     private void addLinkInfo(int socketIndex1, int socketIndex2) {
-        MessagingNodeInfo node1 = messagingNodes[socketIndex1];
-        MessagingNodeInfo node2 = messagingNodes[socketIndex2];
+        MessagingNodeInfo node1 = messagingNodes.get(socketIndex1);
+        MessagingNodeInfo node2 = messagingNodes.get(socketIndex2);
         int weight = new Random().nextInt(10 - 1 + 1) + 1;
         LinkInfo link = new LinkInfo(node1.getHostname(), node1.getPort(), node2.getHostname(), node2.getPort(),
                 weight);
@@ -75,14 +79,14 @@ public class OverlayCreator {
             ArrayList<Integer> toConnect = connections.get(i);
 
             MessagingNodeInfo[] nodeInfos = new MessagingNodeInfo[toConnect.size()];
-            for (int connection = 0; connection < toConnect.size(); connection++) {
-                int nodeIndex = toConnect.get(connection);
-                nodeInfos[i] = messagingNodes[nodeIndex];
+            for (int j = 0; j < toConnect.size(); j++) {
+                int nodeIndex = toConnect.get(j);
+                nodeInfos[j] = messagingNodes.get(nodeIndex);
             }
 
             MessagingNodesList message = new MessagingNodesList(nodeInfos);
 
-            new TCPSender(messagingNodes[i].getSocket()).send(message);
+            new TCPSender(messagingNodes.get(i).getSocket()).send(message);
         }
     }
 }
