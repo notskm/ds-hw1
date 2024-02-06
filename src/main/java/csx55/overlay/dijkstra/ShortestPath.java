272f401869e3dbb87f2b583f6f0fb7995b4b2fc4
notskm <41784014+notskm@users.noreply.github.com>
2024-02-05 21:55:30 -0500

    Fix pathfinding, use new Graph class

--- src/main/java/csx55/overlay/dijkstra/ShortestPath.java
+++ src/main/java/csx55/overlay/dijkstra/ShortestPath.java
@@ -2,50 +2,38 @@ package csx55.overlay.dijkstra;
 
 import csx55.overlay.wireformats.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ShortestPath {
-    Map<MessagingNodeInfo, List<Edge>> graph;
     Map<MessagingNodeInfo, Integer> distances;
     Map<MessagingNodeInfo, MessagingNodeInfo> predecessors;
+    Graph graph;
 
     public ShortestPath(LinkInfo[] links) {
-        graph = new HashMap<>();
+        graph = new Graph(links);
         distances = new HashMap<>();
         predecessors = new HashMap<>();
-
-        for (LinkInfo link : links) {
-            MessagingNodeInfo nodeA = new MessagingNodeInfo(link.getHostnameA(), link.getPortA());
-            MessagingNodeInfo nodeB = new MessagingNodeInfo(link.getHostnameB(), link.getPortB());
-
-            graph.putIfAbsent(nodeA, new ArrayList<>());
-            graph.putIfAbsent(nodeB, new ArrayList<>());
-
-            graph.get(nodeA).add(new Edge(nodeB, link.getWeight()));
-        }
     }
 
     public Map<MessagingNodeInfo, MessagingNodeInfo> computeShortestPaths(MessagingNodeInfo source) {
-        Queue<PriorityNode> queue = new PriorityQueue<>();
-
         distances.put(source, 0);
 
-        for (MessagingNodeInfo vertex : graph.keySet()) {
+        Queue<PriorityNode> queue = new PriorityQueue<>();
+
+        for (MessagingNodeInfo vertex : graph.vertices()) {
             if (!vertex.equals(source)) {
                 distances.put(vertex, Integer.MAX_VALUE);
                 predecessors.put(vertex, null);
             }
-            queue.add(new PriorityNode(vertex, 0));
+            queue.add(new PriorityNode(vertex, distances.get(vertex)));
         }
 
         while (!queue.isEmpty()) {
             MessagingNodeInfo u = queue.remove().get();
-            for (Edge edge : graph.get(u)) {
+            for (Graph.Edge edge : graph.adjacencyList(u)) {
                 int alt = distances.get(u) + edge.weight;
                 if (alt < distances.get(edge.vertex)) {
                     distances.put(edge.vertex, alt);
@@ -56,16 +44,6 @@ public class ShortestPath {
         }
 
         return predecessors;
-    }
-}
-
-class Edge {
-    final public MessagingNodeInfo vertex;
-    final public int weight;
-
-    Edge(MessagingNodeInfo node, int weight) {
-        this.vertex = node;
-        this.weight = weight;
     }
 }
 
