7feed65222b9dc75d2882e2c40994f5691fe62a0
notskm <41784014+notskm@users.noreply.github.com>
2024-02-07 17:56:28 -0700

    Remove unecessary import

--- src/main/java/csx55/overlay/node/Node.java
+++ src/main/java/csx55/overlay/node/Node.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
